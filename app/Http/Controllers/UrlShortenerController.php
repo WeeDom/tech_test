8e8ae3c2368ce899514ea0d4368c5260468ec5b2
Dominic Pain <dominic_pain@hotmail.com>
2025-02-13 15:16:57 +0000

    implement redis cache, rather thanjust using a throwaway variable

--- app/Http/Controllers/UrlShortenerController.php
+++ app/Http/Controllers/UrlShortenerController.php
@@ -3,18 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class UrlShortenerController extends Controller
 {
-    protected static $urlMap = [];
-
     public function encode(Request $request)
     {
         $request->validate(['url' => 'required|url']);
         $originalUrl = $request->input('url');
 
         $shortCode = $this->generateShortCode($originalUrl);
-        self::$urlMap[$shortCode] = $originalUrl;
+        Redis::setex("short:$shortCode", 86400, $originalUrl); // Store in Redis with 1-day expiry
 
         return response()->json(['short_url' => "http://{$shortCode}"]);
     }
@@ -23,15 +22,16 @@ class UrlShortenerController extends Controller
     {
         $request->validate(['short_url' => 'required']);
         $shortUrl = str_replace('http://', '', $request->input('short_url'));
-
-        if (!isset(self::$urlMap[$shortUrl])) {
+        
+        $originalUrl = Redis::get("short:$shortUrl");
+        if (!$originalUrl) {
             return response()->json(['error' => 'Short URL not found'], 404);
         }
 
-        return response()->json(['original_url' => self::$urlMap[$shortUrl]]);
+        return response()->json(['original_url' => $originalUrl]);
     }
 
-private function generateShortCode($url)
+    private function generateShortCode($url)
     {
         $md5Hash = md5($url);
         $base62 = base_convert(substr($md5Hash, 0, 10), 16, 36); // Convert hex to base62-like
